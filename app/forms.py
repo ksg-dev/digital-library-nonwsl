bfc4265e5181f1a841a9efae238c31d582efa21c
ksg-dev <ksg.dev.data@gmail.com>
2024-08-09 11:07:19 -0500

    add edit book form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, SubmitField
-from wtforms.validators import InputRequired, NumberRange
+from wtforms import StringField, DecimalField, SubmitField, HiddenField
+from wtforms.validators import InputRequired, NumberRange, ValidationError
+from app import db
+from models import Book
 
 
 
@@ -23,4 +25,36 @@ class BookForm(FlaskForm):
                           places=1
                         )
     submit = SubmitField("Add Book")
+
+class EditBookForm(FlaskForm):
+    title = StringField('Book Title',
+                        validators=[
+                            InputRequired()
+                        ]
+                        )
+    author = StringField('Author',
+                         validators=[
+                             InputRequired()
+                         ]
+                         )
+    rating = DecimalField('Rating',
+                          validators=[
+                              InputRequired(),
+                              NumberRange(min=0, max=10, message="Please enter a rating between 0.0 and 10.0")
+                          ],
+                          places=1
+                          )
+    submit = SubmitField("Update Record")
+
+    def __init__(self, og_title, key, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.og_title = og_title
+        self.key = key
+
+    def validate_edit_title(self, title):
+        if title.data != self.og_title:
+            book = db.session.execute(db.select(Book).where(Book.title == title.data)).scalars()
+
+            if book is not None:
+                raise ValidationError("Title already exists in database, please pick a different title")
 
